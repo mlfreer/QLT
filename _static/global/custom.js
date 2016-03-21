aac5d9d62934c2315e3c977ca9d124cf6898856b
artdol <artdol@inbox.ru>
2016-03-21 19:54:05 -0400

    Decimals

--- _static/global/custom.js
+++ _static/global/custom.js
@@ -1,19 +1,22 @@
+function toDecimal(num) {
+    return parseFloat(Math.round(num * 100) / 100).toFixed(1);
+}
 
 $(".form-control").change(function(){
 	
 	cashexp=document.getElementById("id_CashQuantity").options[document.getElementById("id_CashQuantity").selectedIndex].value*CashPrice;
-	$("#cashexp").html(cashexp);
+	$("#cashexp").html(toDecimal(cashexp));
 	masonexp=document.getElementById("id_MasonMoneyQuantity").options[document.getElementById("id_MasonMoneyQuantity").selectedIndex].value*MasonMoneyPrice;
-	$("#masonexp").html(masonexp);
+	$("#masonexp").html(toDecimal(masonexp));
 	barnesexp=document.getElementById("id_BarnesNobleQuantity").options[document.getElementById("id_BarnesNobleQuantity").selectedIndex].value*BarnesNoblePrice;
-	$("#barnesexp").html(barnesexp);
+	$("#barnesexp").html(toDecimal(barnesexp));
 	fandangoexp=document.getElementById("id_FandangoQuantity").options[document.getElementById("id_FandangoQuantity").selectedIndex].value*FandangoPrice;
-	$("#fandangoexp").html(fandangoexp);
+	$("#fandangoexp").html(toDecimal(fandangoexp));
 	gapexp=document.getElementById("id_GapQuantity").options[document.getElementById("id_GapQuantity").selectedIndex].value*GapPrice;
-	$("#gapexp").html(gapexp);
+	$("#gapexp").html(toDecimal(gapexp));
 	
 	total_expenditure=cashexp+masonexp+barnesexp+fandangoexp+gapexp;
-	$("#totalexp").html(total_expenditure);
+	$("#totalexp").html(toDecimal(total_expenditure));
 })
 
 $(".input-group-addon").html("%")
