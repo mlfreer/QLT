ca1b1b4eca66217ea07a99d7a3d508c3b34a6977
mlfreer <1991F@inbox.ru>
2016-10-23 14:22:55 -0400

    starting T2: optimal mechanism

---
+++ CollectiveExperimentationT2/views.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+from __future__ import division
+
+from otree.common import Currency as c, currency_range, safe_json
+
+from . import models
+from ._builtin import Page, WaitPage
+from .models import Constants
+
+#first decision stage
+class Decision2Start(Page):
+	form_model = models.Player
+	form_fields = ['VerbalVoteStage1']
+
+	template_name = 'CollectiveExperimentation/Decision2Start.html'
+
+class Signals1WaitPage(WaitPage):
+	def after_all_players_arrive(self):
+		self.player.group.get_start()
+		for p in self.player.group.get_players():
+			p.get_signal1()
+
+		
+	template_name = 'CollectiveExperimentation/Signals1WaitPage.html'
+
+class Decision2Continue(Page):
+
+	def is_displayed(self):
+		return self.player.group.Start
+
+	form_model = models.Player
+	form_fields = ['VerbalVoteStage2']
+
+	template_name = 'CollectiveExperimentation/Decision2Continue.html'
+
+class Signals2WaitPage(WaitPage):
+	def is_displayed(self):
+		return self.player.group.Start
+
+	def after_all_players_arrive(self):
+		self.player.group.get_continue()
+		for p in self.player.group.get_players():
+			p.get_signal2()
+
+
+	template_name = 'CollectiveExperimentation/Signals1WaitPage.html'
+
+
+#deciding whether to implement
+class Decision2Implement(Page):
+	def is_displayed(self):
+		return self.player.group.Continue
+
+	form_model = models.Player
+	form_fields = ['VerbalVoteStage3']
+
+	template_name = 'CollectiveExperimentation/Decision2Implement.html'
+
+class ResultsWaitPage(WaitPage):
+
+	def after_all_players_arrive(self):
+		self.player.group.get_implement()
+		for p in self.player.group.get_players():
+			p.get_payoff()
+
+	template_name = 'CollectiveExperimentation/Signals1WaitPage.html'
+
+
+
+class Results(Page):
+	pass
+
+
+page_sequence = [
+	Decision2Start,
+	Signals1WaitPage,
+	Decision2Continue,
+	Signals2WaitPage,
+	Decision2Implement,
+	ResultsWaitPage,
+	Results
+]
